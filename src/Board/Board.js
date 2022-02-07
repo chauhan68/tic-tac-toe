e7d1dd497edef877750e9ad021f08510fcaf5324
Chauhan <Pritam.Chauhan@unisys.com>
2022-02-07 21:25:52 +0530

    Adding history

--- src/Board/Board.js
+++ src/Board/Board.js
@@ -7,6 +7,7 @@ import calculateWinner from '../utils/gameUtils';
 
 const Board = () => {
     const [squares, setSquares] = useState(Array.from({ length: 9 }, () => ''));
+    const [history, setHistory] = useState([]);
     const [currentValue, updateCurrentValue] = useState('X');
     const [gameWinner, updateGameWinner] = useState('');
 
@@ -21,20 +22,40 @@ const Board = () => {
         updateCurrentValue(updatedCurrentValue);
         const gameWonBy = calculateWinner(updatedArray);
         updateGameWinner(gameWonBy);
+        const updatedMoves = [...history, updatedArray]
+        setHistory(updatedMoves)
     }
 
     const resetGame = () => {
         setSquares(Array.from({ length: 9 }, () => ''));
         updateCurrentValue('X');
         updateGameWinner('');
+        setHistory([])
+    }
+
+    const onHistoryClick = (index) => {
+        const updatedHistory = history.slice(0, index + 1)
+        setHistory(updatedHistory)
+        const squaresToRender = updatedHistory[index] || Array.from({ length: 9 }, () => '')
+        setSquares(squaresToRender)
+        const updatedCurrentValue = index % 2 === 0 ? 'O' : 'X';
+        updateCurrentValue(updatedCurrentValue);
+        const gameWonBy = calculateWinner(history[index]);
+        updateGameWinner(gameWonBy);
+        
     }
 
     return (
-        <div className='board'>
-            <h1>{!gameWinner ? squares.some(square => !square) ? `Next Move: ${currentValue}` : 'It\'s a draw!' : `Winner is ${gameWinner}`} </h1>
-            {squares.map((squareValue, index) => <Square key={index} id={index} value={squareValue} onClick={handleOnClick} />)}
-            <button className='resetButton' onClick={resetGame}>Reset Game</button>
-        </div>
+        <React.Fragment>
+            <div className='board'>
+                <h1>{!gameWinner ? squares.some(square => !square) ? `Next Move: ${currentValue}` : 'It\'s a draw!' : `Winner is ${gameWinner}`} </h1>
+                {squares.map((squareValue, index) => <Square key={index} id={index} value={squareValue} onClick={handleOnClick} />)}
+                <button className='resetButton' onClick={resetGame}>Reset Game</button>
+            </div>
+            <div className='history-list'>
+                {history.map((squares, index) => <button key={index} onClick={() => { onHistoryClick(index) }}>Go to Move {index + 1}</button>)}
+            </div>
+        </React.Fragment>
     )
 };
 
